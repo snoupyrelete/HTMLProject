142fd1523ce711adb39b4406132e9d8b7014bcf3
snoupyrelete <drob8896@740s-pl-112.local>
2016-11-16 08:43:02 -0700

    start

---
+++ src/html/controller/HTMLController.java
@@ -0,0 +1,9 @@
+package html.controller;
+
+public class HTMLController
+{
+	public void start()
+	{
+		//String html = Jsoup.connect("http://stackoverflow.com").get().html();
+	}
+}
---
+++ src/html/controller/HTMLRunner.java
@@ -0,0 +1,11 @@
+package html.controller;
+
+public class HTMLRunner
+{
+	public static void main (String [] args)
+	{
+		HTMLController myHTMLController = new HTMLController();
+		myHTMLController.start();
+		
+	}
+}
